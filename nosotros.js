b69d2079498e81dd842a99da7d754110b7076f85
Ailinn <2336000218@utna.edu.mx>
2026-06-08 17:42:34 -0600

    Agregar la funcionalidad js

--- nosotros.js
+++ nosotros.js
@@ -1,6 +1,11 @@
 function cambiarColor(){
     let colores = [
-        "pink",
-        "lig"
-    ]
+        "lightpink",
+        "lightblue",
+        "lightyellow",
+        "lightpurple"
+
+    ];
+    let aleatorio = Math.floor(Math.random() * colores.length);
+    document.body.style.backgroundColor = colores [aleatorio];
 }
